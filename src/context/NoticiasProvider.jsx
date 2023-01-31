71a8d0a1919ecba2e585a05548ee42656d776dab
Jeison-Vidal <j.vidal93@pascualbravo.edu.co>
2023-01-30 21:27:54 -0500

    working in box commit

---
+++ src/context/NoticiasProvider.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import {useState,useEffect,createContext} from 'react'
+const NoticiasContext= createContext()
+
+const NoticiasProvider=({children})=>{
+    const [categoria,setCategoria]=useState('general')
+    const [noticias,useNoticias]=useState([])
+    const handleChangeCategoria= e =>{
+
+        setCategoria(e.target.value)
+    }
+    useEffect(() => {
+      
+    const consultarAPI= async () =>{
+        const url=`https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&pageSize=100&apiKey=${import.meta.env.VITE_API_KEY}`
+        const {data}= await axios(url)
+        console.log(data)
+
+    }
+      consultarAPI()
+    }, [categoria])
+    
+
+return (
+    <NoticiasContext.Provider value={{categoria,handleChangeCategoria
+    
+    
+    
+    }}>
+        {children}
+
+
+    </NoticiasContext.Provider>
+
+)
+
+}
+
+export {
+    NoticiasProvider
+
+}
+export default NoticiasContext
